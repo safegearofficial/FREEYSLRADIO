d7a9ddeb47fb0f954338597d0db63cf854330582
safegearofficial <109935711+safegearofficial@users.noreply.github.com>
2023-04-10 12:44:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const API_KEY = "AIzaSyBYOdFNlLytvvpH52y4UyZHwHklFN_N2Qg";
 const ARTIST_NAME = "Young Thug";
 const MAX_RESULTS = 25;
 
-let player;
 let currentPlaylist = [];
 let currentTrack = 0;
 
@@ -18,43 +17,20 @@ function onYouTubeApiLoad() {
 
 function searchForMusic() {
   const request = gapi.client.youtube.search.list({
-  part: "snippet,status",
-  type: "video",
-  q: ARTIST_NAME,
-  maxResults: MAX_RESULTS,
-  fields: "items(id(videoId),snippet(publishedAt,channelId,channelTitle,title,description),status(embeddable))"
-});
+    part: "snippet",
+    type: "video",
+    q: ARTIST_NAME,
+    maxResults: MAX_RESULTS,
+    fields: "items(id(videoId),snippet(publishedAt,channelId,channelTitle,title,description))"
+  });
 
   request.execute(onSearchResponse);
 }
 
 function onSearchResponse(response) {
-  const videoIds = response.items
-    .filter(item => item.status.embeddable) // Filter out videos that are not embeddable
-    .map(item => item.id.videoId);
+  const videoIds = response.items.map(item => item.id.videoId);
   currentPlaylist = videoIds;
   playVideo(currentPlaylist[currentTrack]);
-}
-
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player("player", {
-    height: "360",
-    width: "640",
-    events: {
-      "onReady": onPlayerReady,
-      "onStateChange": onPlayerStateChange
-    }
-  });
-}
-
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-function onPlayerStateChange(event) {
-  if (event.data === YT.PlayerState.ENDED) {
-    playNextTrack();
-  }
 }
 
 function playNextTrack() {
@@ -63,9 +39,8 @@ function playNextTrack() {
 }
 
 function playVideo(videoId) {
-  if (player) {
-    player.loadVideoById(videoId);
-  }
+  const youtubePlayer = document.getElementById("youtubePlayer");
+  youtubePlayer.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
 }
 
 window.addEventListener("load", onClientLoad);
